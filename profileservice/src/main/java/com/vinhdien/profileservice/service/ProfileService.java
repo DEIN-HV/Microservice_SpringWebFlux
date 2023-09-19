2de20dacc9840033ac7d9c5d884f9126170c4faf
dien.hv <dien.hv@vn.vinx.asia>
2023-09-19 15:11:44 +0700

    add profile controller

--- profileservice/src/main/java/com/vinhdien/profileservice/service/ProfileService.java
+++ profileservice/src/main/java/com/vinhdien/profileservice/service/ProfileService.java
@@ -1,8 +1,25 @@
 package com.vinhdien.profileservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vinhdien.profileservice.model.ProfileDTO;
+import com.vinhdien.profileservice.repository.ProfileRepository;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 @Service
+@Slf4j
 public class ProfileService {
 
+    @Autowired
+    ProfileRepository profileRepository;
+
+    public Flux<ProfileDTO> getAllProfile() {
+        return profileRepository.findAll()
+                .map(ProfileDTO::entityToDto)
+                .switchIfEmpty(Mono.error(new Exception("error")));
+    }
 }
